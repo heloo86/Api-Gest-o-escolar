63504b088bd8e68dbdefbee1f25044fe5b64adcd
heloo86 <heloisa.af86@gmail.com>
2026-02-26 14:03:41 -0300

    first commit

---
+++ gestao-escolar/src/test/java/com/senai/gestao_escolar/GestaoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.gestao_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
